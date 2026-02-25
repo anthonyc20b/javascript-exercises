0433fea9934963b042bf711b015cb8f6a5fd55f3
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-02-25 10:25:16 -0700

    Create function to modify array for exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...valuesToRemove) {
+    return oldArray.filter(item => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
