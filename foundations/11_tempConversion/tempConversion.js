b4f3b7296f82b6c668ae93d43d707e41b55a6c6a
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-03-04 20:56:02 -0700

    Create conversion functions for exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let temp = (tempF - 32) * 5/9;
+  tempRound = Math.round(temp*10) / 10;
+  return tempRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let temp = (tempC * 9/5) + 32;
+  tempRound = Math.round(temp*10) / 10;
+  return tempRound;
 };
 
 // Do not edit below this line
