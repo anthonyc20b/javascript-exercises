a212143491771c8a97f259271b1d95c78b352855
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-09 10:03:27 -0600

    Add clarification for later use to explain code
    Used comments to describe in more detail the specific code decisions
    that were made and how they worked for later reference.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
 const palindromes = function (string) {
-
+// Below two lines first take input of string above and remove special characters, convert to lower case, then split
+// the array into seperate letters, reverse the array, and join back the array to create a new string.
     let caseSpecific = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
     let newString = caseSpecific.split("").reverse().join("");
 
