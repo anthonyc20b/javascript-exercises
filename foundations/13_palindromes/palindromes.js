21a0ffe06678789f42cd2ec93892856b5f8a688b
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-09 10:02:24 -0600

    Create function to detect palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let caseSpecific = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    let newString = caseSpecific.split("").reverse().join("");
+
+    if (newString === caseSpecific) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
