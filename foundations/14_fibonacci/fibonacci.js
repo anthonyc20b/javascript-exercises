e47f20fcc42c14a698c340b0bda815b6b595e08e
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-04-09 10:22:02 -0600

    Add in edge cases to prevent program errors

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n <= -1) {
+        return "OOPS";
+    }
+    if (n === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < n; i++) {
+        
+    };
 
 };
 
